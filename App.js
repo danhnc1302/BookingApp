fb70f1caadadcd83203a8b760ffd21a56523657c
danhnc <danhnc1302@gmail.com>
2024-01-06 16:14:43 +0700

    finish

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { ModalPortal } from 'react-native-modals';
 import Navigation from "./navigation";
-
+import store from './redux/store';
+import { Provider } from 'react-redux';
 export default function App() {
   return (
-    <>
+    <Provider store={store}>
       <Navigation />
       <ModalPortal />
-    </>
+    </Provider>
   );
 }
--- navigation/StackNavigator.js
+++ navigation/StackNavigator.js
@@ -4,6 +4,10 @@ import SearchScreen from "../screens/SearchScreen";
 import PlacesScreen from "../screens/PlacesScreen";
 import MapScreen from "../screens/MapScreen";
 import InforScreen from "../screens/InforScreen";
+import RoomsScreen from "../screens/RoomsScreen";
+import UserScreen from "../screens/UserScreen";
+import ComfirmationScreen from "../screens/ComfirmationScreen";
+import BookingScreen from "../screens/BookingScreen";
 const Stack = createNativeStackNavigator()
 
 export default function StackNavigator() {
@@ -14,8 +18,10 @@ export default function StackNavigator() {
             <Stack.Screen name="Places" component={PlacesScreen} options={{ headerShown: false }} />
             <Stack.Screen name="Map" component={MapScreen} options={{ headerShown: false }} />
             <Stack.Screen name="Info" component={InforScreen} options={{ headerShown: false }} />
-
-
+            <Stack.Screen name="Rooms" component={RoomsScreen} options={{ headerShown: false }} />
+            <Stack.Screen name="User" component={UserScreen} options={{ headerShown: false }} />
+            <Stack.Screen name="Confirm" component={ComfirmationScreen} options={{ headerShown: false }} />
+            <Stack.Screen name="Booking" component={BookingScreen} options={{ headerShown: false }} />
         </Stack.Navigator>
     )
 }
